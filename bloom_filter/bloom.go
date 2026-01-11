1746e00f56e4728b7b8b8406a92a6c58bca2bc8e
Alex Yersh <xyersh@gmail.com>
2026-01-11 22:43:46 +0300

    - дописал функционал для Блума - нарисовал презенташку для Блума

--- bloom_filter/bloom.go
+++ bloom_filter/bloom.go
@@ -2,13 +2,14 @@ package bloom_filter
 
 import (
 	"math"
-	// "github.com/spaolacci/murmur3"
+
+	"github.com/spaolacci/murmur3"
 	"github.com/xyersh/xuyacs/bitarray"
 )
 
 type BloomFilterI interface {
-	Add([]byte)
-	Test([]byte) bool
+	Add([]byte)       // добавить отпечаток знаяения в фильтр Блума
+	Test([]byte) bool // проверить наличие отпечатка в фильтре Блума (возможен ложно-положительный результат )
 }
 
 type Bitmap []bitarray.BitArray
@@ -22,8 +23,12 @@ type BloomFilter struct {
 
 // NewBloomFilter создает новый фильтр Блума с заданным количеством элементов и вероятностью ложного срабатывания.
 // n - ожидаемое количество элементов для анализа
-// p - вероятность ложного срабатывания (доя от единицы)
+// p - вероятность ложного срабатывания ( от нуля  до единицы)
 func NewBloomFilter(n int, p float64) *BloomFilter {
+	if p < 0.0 || p > 1.0 {
+		panic("`p` must be between 0.0 and 1.0")
+	}
+
 	m := uint(math.Ceil(-float64(n) * math.Log(p) / math.Pow(math.Log(2), 2)))
 	k := uint(math.Ceil(float64(m) / float64(n) * math.Log(2)))
 
@@ -33,7 +38,7 @@ func NewBloomFilter(n int, p float64) *BloomFilter {
 
 	bitsArr := make(Bitmap, k)
 	for i := 0; i < int(k); i++ {
-		bitsArr[i] = *bitarray.NewBitArray(int(m))
+		bitsArr[i] = *bitarray.NewBitArray(m)
 	}
 
 	return &BloomFilter{
@@ -44,9 +49,31 @@ func NewBloomFilter(n int, p float64) *BloomFilter {
 
 }
 
+func (bf *BloomFilter) hash128(data []byte) (uint, uint) {
+	h1, h2 := murmur3.Sum128(data)
+	return uint(h1), uint(h2)
+}
+
 func (bf *BloomFilter) Add(data []byte) {
+	h1, h2 := bf.hash128(data)
+
+	for i := 0; i < int(bf.k); i++ {
+		index := (h1 + uint(i)*h2) % bf.m
+
+		bf.bitmap[i].Set(index, true)
+	}
 }
 
 func (bf *BloomFilter) Test(data []byte) bool {
-	return false
+	h1, h2 := bf.hash128(data)
+
+	for i := 0; i < int(bf.k); i++ {
+		index := (h1 + uint(i)*h2) % bf.m
+
+		if !bf.bitmap[i].Get(index) {
+			return false
+		}
+	}
+
+	return true
 }
--- bloom_filter/test/main.go
+++ bloom_filter/test/main.go
@@ -7,7 +7,18 @@ import (
 )
 
 func main() {
-	bf := bloom_filter.NewBloomFilter(1000000, 5.0)
-	fmt.Println(bf)
+	bf := bloom_filter.NewBloomFilter(100, 0.5)
 
+	bf.Add([]byte("Sasha"))
+	bf.Add([]byte("Masha"))
+	bf.Add([]byte("Dasha"))
+	bf.Add([]byte("Natasha"))
+
+	fmt.Printf("Sasha in filter: %t\n", bf.Test([]byte("Sasha")))
+	fmt.Printf("Masha in filter: %t\n", bf.Test([]byte("Masha")))
+	fmt.Printf("Dasha in filter: %t\n", bf.Test([]byte("Dasha")))
+	fmt.Printf("Natasha in filter: %t\n", bf.Test([]byte("Natasha")))
+
+	fmt.Printf("Lesha in filter: %t\n", bf.Test([]byte("Lesha")))
+	fmt.Printf("Kesha in filter: %t\n", bf.Test([]byte("Kesha")))
 }
